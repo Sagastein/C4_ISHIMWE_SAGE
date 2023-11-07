634b20bdfbeb749ff7f89dabf7b12a24e40075c0
Sagastein <ishimwe.sage.3@gmail.com>
2023-11-07 20:54:47 +0200

    Initialize project start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import "./App.css";
+function App() {
+  return (
+    <main>
+      <h1>Hello word</h1>
+      <p>I am Ishimwe Sage This Is My First React Application</p>
+    </main>
+  )
+}
+
+export default App
