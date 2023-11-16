6f7b28d86fa4e3355f63978135356e034a05476c
Ishimwe Sage <72102004+Sagastein@users.noreply.github.com>
2023-11-16 19:29:53 +0200

    user profile app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
+import UserPortfolio from "./Components/UserPortfolio"
 import "./App.css";
 function App() {
   return (
     <main>
-      <h1>Hello word</h1>
-      <p>I am Ishimwe Sage This Is My First React Application</p>
+     <UserPortfolio/>
     </main>
   )
 }
---
+++ src/Componets/UserPortifolio.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function UserPortfolio() {
+  return (
+    <div>
+      <h1>John Doe</h1>
+      <h2>Educational Background</h2>
+      <ul>
+        <li>Bachelor of Science in Computer Science, University of California, Berkeley</li>
+        <li>Master of Science in Computer Science, Stanford University</li>
+      </ul>
+      <h2>Professional Background</h2>
+      <ul>
+        <li>Software Engineer, Google</li>
+        <li>Data Scientist, Facebook</li>
+      </ul>
+      <h2>Skills</h2>
+      <ul>
+        <li>Programming Languages: Python, Java, C++, JavaScript</li>
+        <li>Frameworks and Libraries: React, TensorFlow, scikit-learn</li>
+        <li>Operating Systems: Linux, macOS, Windows</li>
+      </ul>
+    </div>
+  );
+}
+
+export default UserPortfolio;
